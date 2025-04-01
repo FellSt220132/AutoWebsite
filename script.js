6fc689273c1ca02a8a1f92f5119877354d2a4775
Stefan Fellner <40146720220132@litec.ac.at>
2025-04-01 19:02:51 +0200

    feat: diashow

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+const carouselWrapper = document.querySelector('.carousel-wrapper');
+const carouselImages = document.querySelectorAll('.carousel-image');
+let currentIndex = 0;
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+function moveToNextImage() {
+    currentIndex++;
+    if (currentIndex >= carouselImages.length) {
+        currentIndex = 0;
+    }
+    carouselWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function moveToPreviousImage() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = carouselImages.length - 1;
+    }
+    carouselWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(moveToNextImage, 4000);
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+prevButton.addEventListener('click', moveToPreviousImage);
+nextButton.addEventListener('click', moveToNextImage);
